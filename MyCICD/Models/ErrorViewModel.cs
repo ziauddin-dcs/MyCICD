1354ae528216bd8765abfb4f7c747d128e4ac714
Ziauddin-Hungama <ziauddinhungama@gmail.com>
2025-06-28 17:27:11 +0530

    Add project files.

---
+++ MyCICD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
